40913f0d3a2a86fad123e15dd094ecd926c3bc35
Arjun Busani <v3trax@gmail.com>
2018-09-24 10:28:40 -0400

    https-cors

--- simple/ads.js
+++ simple/ads.js
@@ -39,7 +39,7 @@ function setUpIMA() {
 
   // Request video ads.
   var adsRequest = new google.ima.AdsRequest();
-  adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?' +
+  adsRequest.adTagUrl = 'http://pubads.g.doubleclick.net/gampad/ads?' +
       'sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&' +
       'impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&' +
       'cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=';
